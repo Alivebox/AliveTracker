dba990fa46673113bd878d12b805ea8748b18082
Andres <andresd.garciag@gmail.com>
2013-02-18 08:05:09 -0600

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	app/view/Users/UsersGrid.js
    	app/view/group/GroupDetail.js

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ Ext.application({
         'MainController',
         'home.HomeController',
         'group.GroupDetailController',
+        'group.AddUsersGroupController',
         'reports.ReportsController',
         'projects.LogBookController',
         'authentication.LoginController',
